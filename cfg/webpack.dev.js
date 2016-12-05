126c0e947201bb9ff1dc593dd0f436282e6446a5
luna825 <luna825@qq.com>
2016-12-05 14:58:00 +0800

    config bootstrap

--- cfg/webpack.dev.js
+++ cfg/webpack.dev.js
@@ -13,7 +13,10 @@ var plugins = [
 
 
 var config = {
-  entry:[ path.resolve(projectRootPath, 'src', 'app.js') ],
+  entry:[ 
+    'bootstrap-loader',
+    path.resolve(projectRootPath, 'src', 'app.js') 
+  ],
   output:{
     path:path.resolve(projectRootPath, 'public', 'assets'),
     filename: 'bundle.js',
@@ -29,7 +32,20 @@ var config = {
           presets:['react', 'es2015', 'stage-0'],
           plugins:['transform-decorators-legacy']
         } 
-      }
+      },
+      {
+        test: /\.css$/,loader:'style-loader!css-loader'
+      },
+      {
+        test:/\.scss/,
+        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded'
+      },
+      {
+        test:/\.sass/,
+        loader: 'style-loader!css-loader!sass-loader?outputStyle=expanded&indentedSyntax'
+      },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+      { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
     ]
   },
   resolve:{
